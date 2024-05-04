ffce79da8b9af8efe01f38c917a0567803ccb4dd
Albert Alvarez Estelles <114157589+kukapu@users.noreply.github.com>
2024-05-04 17:31:04 +0200

    11 - WIP

--- 04-teslo-shop/src/products/products.module.ts
+++ 04-teslo-shop/src/products/products.module.ts
@@ -3,13 +3,14 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { Product } from './entities/product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductImage } from './entities/product-image.entity';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
   imports: [
     // Aquí importamos el módulo TypeOrmModule que hemos creado en el archivo products.module.ts
-    TypeOrmModule.forFeature([Product]),
+    TypeOrmModule.forFeature([Product, ProductImage]),
   ],
 })
 export class ProductsModule {}
